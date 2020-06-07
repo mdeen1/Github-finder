6c8ebc548db4ce5b6b67de7a977345581ed630df
Mustapha Deen <mdeen3@outlook.com>
2020-06-07 13:02:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  //Get input text
+  const userText = e.target.value;
+  if(userText !== ''){
+    console.log(userText)
+  }
+});
