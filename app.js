10bd56b371a24ea1efde12d79b49bb31030cd394
Mustapha Deen <mdeen3@outlook.com>
2020-06-14 09:39:46 -0400

    rendered the repos on the page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Init Github = new Github;
 const github = new Github;
+//Init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -14,11 +16,15 @@ searchUser.addEventListener('keyup', (e) => {
     .then(data => {
       if(data.profile.message === 'Not Found') {
         //Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
         // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     })
   } else {
     //Clear profile
+    ui.clearProfile();
   }
 });
